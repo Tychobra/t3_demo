5564ea33bc48e53c95e42ad0deb9b44573ec8770
RichardHHill <richard_hill@brown.edu>
2019-08-22 13:51:10 -0400

    Save data to sqlite in shiny_app/data folder

--- data_prep/demo_data_init.R
+++ data_prep/demo_data_init.R
@@ -19,16 +19,20 @@ hours <- hours %>%
       sample(c("admin", "research", "marketing"), nrow(hours), replace = TRUE),
       paste0(client_short_name, "_", sample(c("proj_1", "proj_2", "proj_3"), nrow(hours), replace = TRUE))
     ),
-    description = sample(c("Bug fixes", "Updated tables", "Added charts", "Prepped Data"), nrow(hours), replace = TRUE)
+    description = sample(c("Bug fixes", "Updated tables", "Added charts", "Prepped Data"), nrow(hours), replace = TRUE),
+    date = as.character(date)
   )
 
-
-conn2 <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+conn2 <- DBI::dbConnect(
+  RSQLite::SQLite(), 
+  dbname = "shiny_app/data/hours_db.sqlite3"
+)
 
 DBI::dbWriteTable(
   conn2,
-  "hours",
-  hours
+  "hours2",
+  hours,
+  overwrite = TRUE
 )
 
 DBI::dbListTables(conn2)
